71e02e4ddc3542e77de4b4341b728fd386fef990
zhanghoa <42800990+zhanghoa@users.noreply.github.com>
2020-06-15 16:45:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,14 +13,3 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/coolsnowwolf/packages
-git clone https://github.com/kenzok8/small
-git clone https://github.com/kenzok8/openwrt-packages
-git clone https://github.com/Lienol/openwrt-package
-git clone https://github.com/fw876/helloworld
-git clone https://github.com/openwrt-develop/luci-theme-atmaterial
-git clone https://github.com/vernesong/OpenClash
-git clone https://github.com/tty228/luci-app-serverchan
-git clone https://github.com/rufengsuixing/luci-app-adguardhome
-git clone https://github.com/pymumu/luci-app-smartdns
-git clone https://github.com/jerrykuku/luci-theme-argon
